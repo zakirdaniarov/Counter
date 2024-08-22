c7143e01256a8f023f9cc098d9e156317a613b9e
zakir <zakir95olymp@gmail.com>
2024-08-22 15:34:21 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [value, setValue] = useState(0);
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{value}</h1>
+        <button className="minus" onClick={()=>setValue(value-1)}>- Минус</button>
+        <button className="plus" onClick={()=> setValue(value+1)}>Плюс +</button>
       </div>
     </div>
   );
